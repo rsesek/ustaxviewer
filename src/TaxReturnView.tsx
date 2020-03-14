668364208497139cd308b66c428499430df30a00
Robert Sesek <rsesek@bluestatic.org>
2020-03-14 09:01:32 -0400

    Add license information.

--- src/TaxReturnView.tsx
+++ src/TaxReturnView.tsx
@@ -1,3 +1,8 @@
+// Copyright 2020 Blue Static <https://www.bluestatic.org>
+// This program is free software licensed under the GNU General Public License,
+// version 3.0. The full text of the license can be found in LICENSE.txt.
+// SPDX-License-Identifier: GPL-3.0-only
+
 import { createMemo, createState } from 'solid-js';
 import { For } from 'solid-js/dom';
 import { Form, TaxReturn } from 'ustaxlib/core';
